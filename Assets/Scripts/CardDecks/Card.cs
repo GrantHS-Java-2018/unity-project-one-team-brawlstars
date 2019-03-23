48a6a0283c681f5addcff273ad4e96d4bd56a5a2
Ethan <esw@halfangle.com>
2019-03-23 13:26:21 -0700

    Card Scripts

--- Assets/Scripts/CardDecks/Card.cs
+++ Assets/Scripts/CardDecks/Card.cs
@@ -1,14 +1,14 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
-using System.Runtime.CompilerServices;
 using UnityEngine;
 
-public class PlaceHolderScript : MonoBehaviour
+public class Card : MonoBehaviour
 {
+
     // Start is called before the first frame update
     void Start()
     {
-   
+        
     }
 
     // Update is called once per frame
--- Assets/Scripts/CardDecks/CardDeck.cs
+++ Assets/Scripts/CardDecks/CardDeck.cs
@@ -1,14 +1,13 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
-using System.Runtime.CompilerServices;
 using UnityEngine;
 
-public class PlaceHolderScript : MonoBehaviour
+public class CardDeck : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
     {
-   
+        
     }
 
     // Update is called once per frame
