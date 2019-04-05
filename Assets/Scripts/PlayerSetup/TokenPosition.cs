05f0fcfcf9498716e03425314eccc7f317ad3284
Ethan <esw@halfangle.com>
2019-04-05 15:28:25 -0700

    StartScene TokenPosition script started

---
+++ Assets/Scripts/PlayerSetup/TokenPosition.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+namespace PlayerSetup
+{
+    public class TokenPosition : MonoBehaviour
+    {
+        private Vector3[] _tokenPositions = new Vector3[8];
+        
+        // Start is called before the first frame update
+        void Start()
+        {
+            Vector3 oldPosition = new Vector3(-18f, 18f, 1);
+            for (int n = 0; n < 8; n++)
+            {
+                Vector3 tempPosition;
+                tempPosition = new Vector3(oldPosition.x + n * 3, 18f, 1f);
+                _tokenPositions[n] = tempPosition;
+                gameObject.transform.GetChild(n).transform.position = _tokenPositions[n];
+            }
+        }
+    }
+}
