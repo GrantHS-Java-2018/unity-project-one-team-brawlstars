c6897c9ce903a2764b467f1d6386b68a6b4a55c6
Ethan Wickman <esw@halfangle.com>
2019-04-05 20:56:51 -0700

    No Notable Changes

--- Assets/Scripts/PlayerSetup/TokenPosition.cs
+++ Assets/Scripts/PlayerSetup/TokenPosition.cs
@@ -9,13 +9,12 @@ namespace PlayerSetup
         // Start is called before the first frame update
         void Start()
         {
-            Vector3 oldPosition = new Vector3(-18f, 18f, 1);
-            for (int n = 0; n < 8; n++)
+            Vector3 oldPosition = new Vector3(-25.95f, 10f, 0);
+            for (int n = 0; n < 4; n++)
             {
-                Vector3 tempPosition;
-                tempPosition = new Vector3(oldPosition.x + n * 3, 18f, 1f);
+                Vector3 tempPosition = new Vector3((float) (oldPosition.x + n * 8.65), 0f, 0f);
                 _tokenPositions[n] = tempPosition;
-                gameObject.transform.GetChild(n).transform.position = _tokenPositions[n];
+                gameObject.transform.GetChild(n).position = _tokenPositions[n];
             }
         }
     }
