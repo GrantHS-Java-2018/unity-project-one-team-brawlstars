36a5326cba0d89d4c27b57e2a9ff0945264d0952
Ethan <esw@halfangle.com>
2019-04-10 15:30:27 -0700

    Edits to player setup/selection

--- Assets/Scripts/PlayerSetup/TokenPosition.cs
+++ Assets/Scripts/PlayerSetup/TokenPosition.cs
@@ -5,16 +5,25 @@ namespace PlayerSetup
     public class TokenPosition : MonoBehaviour
     {
         private Vector3[] _tokenPositions = new Vector3[8];
+
+        public GameObject tokens;
         
         // Start is called before the first frame update
         void Start()
         {
-            Vector3 oldPosition = new Vector3(-25.95f, 10f, 0);
             for (int n = 0; n < 4; n++)
             {
-                Vector3 tempPosition = new Vector3((float) (oldPosition.x + n * 8.65), 0f, 0f);
+                Vector3 tempPosition = new Vector3( -25.95f + n * 8.65f, 10f, 0f);
                 _tokenPositions[n] = tempPosition;
-                gameObject.transform.GetChild(n).position = _tokenPositions[n];
+                tokens.transform.GetChild(n).position = _tokenPositions[n];
+                tokens.transform.GetChild(3).position = new Vector3(0f, 0f, 0f);
+            }
+
+            for (int n = 4; n < 8; n++)
+            {
+                Vector3 tempPosition = new Vector3( -25.95f + (n - 4) * 8.65f, -2f, 0f);
+                _tokenPositions[n] = tempPosition;
+                tokens.transform.GetChild(n).position = _tokenPositions[n];
             }
         }
     }
