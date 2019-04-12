34e88d94d2e51a6ad0fef81ecbb46cdbc6854d40
Ethan <esw@halfangle.com>
2019-04-12 15:28:26 -0700

    Token Positions Done

--- Assets/Scripts/PlayerSetup/TokenPosition.cs
+++ Assets/Scripts/PlayerSetup/TokenPosition.cs
@@ -6,24 +6,26 @@ namespace PlayerSetup
     {
         private Vector3[] _tokenPositions = new Vector3[8];
 
-        public GameObject tokens;
+        private GameObject _tokens;
         
         // Start is called before the first frame update
         void Start()
         {
             for (int n = 0; n < 4; n++)
             {
-                Vector3 tempPosition = new Vector3( -25.95f + n * 8.65f, 10f, 0f);
+                Vector3 tempPosition = new Vector3( -12f + n * 8f, 5f, 0f);
                 _tokenPositions[n] = tempPosition;
-                tokens.transform.GetChild(n).position = _tokenPositions[n];
-                tokens.transform.GetChild(3).position = new Vector3(0f, 0f, 0f);
             }
 
             for (int n = 4; n < 8; n++)
             {
-                Vector3 tempPosition = new Vector3( -25.95f + (n - 4) * 8.65f, -2f, 0f);
+                Vector3 tempPosition = new Vector3( -12f + (n - 4) * 8f, -1f, 0f);
                 _tokenPositions[n] = tempPosition;
-                tokens.transform.GetChild(n).position = _tokenPositions[n];
+            }
+
+            for (int n = 0; n < 8; n++)
+            {
+                _tokens.transform.GetChild(n).position = _tokenPositions[n];
             }
         }
     }
