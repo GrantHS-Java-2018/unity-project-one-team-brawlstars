73a4e361e26666c2f5143e803eb771fb25f80fd5
Ethan <esw@halfangle.com>
2019-05-02 15:28:05 -0700

    Monopoly Tile, Gameloop, and Property scripts created
    Add the rest of tiles and work on gameloop

---
+++ Assets/Scripts/TileScripts/GameLoop.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace TileScripts
+{
+    public class GameLoop : MonoBehaviour
+    {
+        [SerializeField] Tile[] gameTrack = new Tile[40];
+    }
+}
