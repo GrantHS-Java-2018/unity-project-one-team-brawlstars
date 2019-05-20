fd0b80b043a33d8a469442644ef55db8eee35eed
Connor Heath <cmheath2303@yahoo.com>
2019-05-20 15:30:16 -0700

    cards

---
+++ Assets/Scripts/CardScripts/CC.cs
@@ -0,0 +1,12 @@
+namespace Scripts.CardScripts
+{
+    public class CC
+    {
+        public class Advance : Card
+        {
+            private readonly int tileToAdvance;
+            public Advance(int _tileToAdvance);
+
+        }
+    }
+}
---
+++ Assets/Scripts/CardScripts/Card.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Scripts.CardScripts
+{
+    public abstract class Card : MonoBehaviour
+    {
+
+        public int cat { get; set; }
+        public abstract int activate(int[] amount);
+        public abstract int activate();
+        public abstract int activate(int cardMoney);
+    }
+
+}
+
+
