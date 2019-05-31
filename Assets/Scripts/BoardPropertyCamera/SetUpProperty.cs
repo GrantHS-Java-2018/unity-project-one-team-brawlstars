f2104d7379f459ab9b4c27258afdae3eff5eb9b7
Cael roberts <itscael@gmail.com>
2019-05-31 15:15:51 -0700

    Property camera implemented

---
+++ Assets/Scripts/BoardPropertyCamera/SetUpProperty.cs
@@ -0,0 +1,24 @@
+﻿/*using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SetUpProperty : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    public GameObject pcamera;
+    void Start()
+    {
+        GameObject.Find("Property Camera").transform.position = new Vector3(0, 2000, 0);
+        PropertyCamera.depth = Camera.main.depth + 1;
+        dieManager.GetComponent<DieManagement>().num2 = dieSideThrown;
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
+*/
