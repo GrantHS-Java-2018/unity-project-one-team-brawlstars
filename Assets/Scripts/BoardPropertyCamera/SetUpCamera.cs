63f3aec968d8795b9b5d33a3f49903abae51635e
Ethan <esw@halfangle.com>
2019-06-06 15:17:33 -0700

    Merge branch 'PropertyCamera'

---
+++ Assets/Scripts/BoardPropertyCamera/SetUpCamera.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SetUpCamera : MonoBehaviour {
+    public Camera MainCamera;
+    public Camera PropertyCamera;
+    public GameObject propManager;
+
+    private void Start()
+    {
+        MainCamera.enabled = true;
+        PropertyCamera.enabled = false;
+    }
+
+    public void ShowBoard() {
+        MainCamera.enabled = true;
+        PropertyCamera.enabled = false;
+        
+    }
+    
+    public void ShowProperty() {
+        MainCamera.enabled = false;
+        PropertyCamera.enabled = true;
+        propManager.GetComponent<SetUpProperty>().loadProps();
+    }
+}
---
+++ Assets/Scripts/BoardPropertyCamera/SetUpProperty.cs
@@ -0,0 +1,36 @@
+﻿/*using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SetUpProperty : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    private Sprite[] Props;
+    private SpriteRenderer rend;
+    private Vector3 tempPosition1 = new Vector3( -768f * 512f, 450f, 0f);
+    void Start()
+    {
+        rend = GetComponent<SpriteRenderer>();
+        Props = Resources.LoadAll<Sprite>("Sprites/Deeds/");
+        rend.sprite = Props[22];  
+
+    }
+
+    private void loadProps()
+    {
+        rend.sprite = Props[3];
+        Props[3].transform.position = tempPosition1;
+       
+    }
+    
+    
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+        
+    }
+}
+*/
